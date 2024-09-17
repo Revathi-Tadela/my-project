e5b29d8851094fb26109b5e4e467bb167f20ed0a
revathi <tadelarevathi1989@gmail.com>
2024-09-18 00:49:30 +1000

    Initial commit

---
+++ script1.py
@@ -0,0 +1,1 @@
+print('Hello, World'c:/Users/revat/OneDrive/Desktop/Guvi-Devops/docker-compose.yaml')
---
+++ script2.sh
@@ -0,0 +1,1 @@
+echo 'Hello, World'c:/Users/revat/OneDrive/Desktop/Guvi-Devops/docker-compose.yaml'
